dbc09cddd2b90fe05243645c08395f218d380c32
MichalZin <124308169+MichalZin@users.noreply.github.com.>
2024-07-29 13:34:57 +0200

    Clean up of js functions

--- script.js
+++ script.js
@@ -9,34 +9,60 @@ const bookRead = document.getElementById('readStatus');
 const bookSubmit = document.getElementById('submit');
 const bookList = document.getElementById('bookList');
 
+        
+
 function Book(title, author, pagesCount, read) {
     this.title = title;
     this.author = author;
     this.pagesCount = pagesCount;
     this.read = read; 
+
   // the constructor...
 }
 function addBookToLibrary() {
     bookSubmit.addEventListener('click', (event) => {
       event.preventDefault();
+
+          const titleB =  bookTitle.value;
+          const authorB = bookAuthor.value;
+          const pagesB = bookPage.value;
+          const readB = bookRead.value;
+          const newBook = new Book(titleB, authorB, pagesB, readB);
+
+          const titleField = document.createElement('p');
+          const authorField = document.createElement('p');
+          const pagesField = document.createElement('p');
+          const readField = document.createElement('button');
+          const deleteButton = document.createElement('button');
+
       
-      const newBook = new Book(bookTitle.value, bookAuthor.value, bookPage.value, bookRead.value);
-      
-      const titleField = document.createElement('p');
       titleField.textContent = `${newBook.title}`;
-      bookList.appendChild(titleField);
-      
-      const authorField = document.createElement('p');
       authorField.textContent = `${newBook.author}`;
-      bookList.appendChild(authorField);
-      
-      const pagesField = document.createElement('p');
       pagesField.textContent = `${newBook.pagesCount}`;
-      bookList.appendChild(pagesField);
       
-      const readField = document.createElement('p');
+      bookList.appendChild(titleField);
+      bookList.appendChild(authorField);
+      bookList.appendChild(pagesField);
+
+
+      if (newBook.read === 'Yes') {
       readField.textContent = `${newBook.read}`;
       bookList.appendChild(readField);
+
+      } else {
+        readField.textContent = "Mark as read"
+        bookList.appendChild(readField);
+
+        readField.addEventListener('click', () => 
+          readField.textContent = "Yes")
+
+      }
+      
+      deleteButton.textContent = "Delete position"
+      bookList.appendChild(deleteButton)
+      
+
+      
     });
   };
 
